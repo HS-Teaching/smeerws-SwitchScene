ca34eb932e6929eae18265a8011097e745bfd332
smeerws <hello@smeerws.at>
2020-11-18 21:47:05 +0100

    add second scene switch script without code smell, add scenes which implement the second script (4-6), restruct folders Assets-->MyGame

---
+++ Assets/MyGame/Scripts/SceneLoaderWithParameter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderWithParameter : MonoBehaviour
+{
+    public void LoadNextScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
